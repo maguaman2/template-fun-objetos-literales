b1b5597cb2c9dc703babe06bec07c4a6544986e8
Marco Guamán Buestán <59964049+maguaman2@users.noreply.github.com>
2023-07-24 11:49:44 -0500

    Update validar-vacuna.js

--- validar-vacuna.js
+++ validar-vacuna.js
@@ -1,1 +1,4 @@
-
+//Crear una función que reciba un objeto literal con nombre y edad de una persona, 
+//la función debe validar si la edad aceptado para recibir la vacuna que debe ser 
+//para menores a 5 anos o mayor a 65. La salida de la función es un string de
+//aceptado y no aceptado.
