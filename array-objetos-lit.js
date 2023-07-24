bf939306c04cf3b17c07ad893622945d1c833704
Marco Guamán Buestán <59964049+maguaman2@users.noreply.github.com>
2023-07-24 11:35:33 -0500

    Update array-objetos-lit.js

--- array-objetos-lit.js
+++ array-objetos-lit.js
@@ -1,1 +1,8 @@
-
+//Crear un array con objetos literales, pasar a una funcion como parametro e imprimir la infomracion 
+//en pantalla cada elemento del array.
+//ejemplo:
+//const estudiantes = [{nombre: "Juan", edad: 34},{nombre: "Luis", edad: 25}];
+//Salida:
+//Mi nombre es Juan y tengo 34 anios
+//Mi nombre es Luis y tengo 25 anios
+//Nota: Usar FOR
