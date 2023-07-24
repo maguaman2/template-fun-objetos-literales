aafe94923194179b8e3615d7e35a4e4b8fe175b6
Marco Guamán Buestán <59964049+maguaman2@users.noreply.github.com>
2023-07-24 11:13:14 -0500

    Update hoja-vida.js

--- hoja-vida.js
+++ hoja-vida.js
@@ -1,1 +1,2 @@
-
+//Crear variables con diferentes tipos de datos (number, string, array, objetos literales) para almacenar información de una persona, 
+//pasar estos como parámetros de una función, que imprimirá en pantalla usando un template string con backcstick (``).
